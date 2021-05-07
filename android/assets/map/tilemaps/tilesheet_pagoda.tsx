0ed7a7975cec00ebceded0db0a469ec7bc8ba291
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-05-08 01:29:45 +0300

    добавил новую карту улучшил иерархию файлов в директории карт

--- android/assets/map/tilemaps/tilesheet_pagoda.tsx
+++ android/assets/map/tilemaps/tilesheet_pagoda.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="tilesheet_pagoda" tilewidth="16" tileheight="16" tilecount="224" columns="16">
- <image source="tilesets/Maytch 16x16 Tilesets/tilesheet_pagoda.png" width="256" height="224"/>
+ <image source="../tilesets/Maytch 16x16 Tilesets/tilesheet_pagoda.png" width="256" height="224"/>
 </tileset>
